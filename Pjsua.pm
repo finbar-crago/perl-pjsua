1502b1d3775143a03df74327e641e6f2e1892091
Finbar Crago <finbar.crago@gmail.com>
2014-10-24 01:27:28 +1100

    start perl OO stuff...

--- Pjsua.pm
+++ Pjsua.pm
@@ -14,6 +14,27 @@ our $VERSION = '0.01';
 require XSLoader;
 XSLoader::load('Pjsua', $VERSION);
 
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = {
+        host => $args->{host},
+        user => $args->{user},
+        pass => $args->{pass},
+        port => $args->{port} || '5060',
+    };
+    start();
+    $self->{acc} = add_account_xs($self->{host}, $self->{user}, $self->{pass});
+    return bless $self, $class;
+}
+
+sub call {
+    my ($self, $extn) = @_;
+    my $uri = 'sip:'.$extn.'@'.$self->{host};
+    return call_xs($self->{acc}, $uri);
+}
+
+
 1;
 
 __END__
--- test.pl
+++ test.pl
@@ -2,6 +2,7 @@
 use ExtUtils::testlib;
 use Pjsua;
 
-Pjsua::start();
-my $acc_id = Pjsua::add_account_xs("192.168.2.1", "user", "pass");
-print Pjsua::call_xs($acc_id, "sip:123@192.168.2.1");
+my $test = Pjsua->new({host => "192.168.2.1", user => "user", pass => "pass"});
+$test->call('1234');
+
+while(1){}
